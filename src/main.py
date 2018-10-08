399f85107d833c790fddb62b4b8ff26bb17fd27d
laurence <dev@lkeijzer.nl>
2018-10-08 20:11:53 +0200

    implemented

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 import re
+from src import constants
 
 
 def main():
-    pass
+    with open(constants.filename, 'r') as file:
+        # Get the words as an array
+        words = file.read().split('\n')
+
+        # Get the regex
+        r = re.compile(constants.regex_banned_letters, re.IGNORECASE)
+
+        # Process the list
+        passed_words = list(filter(r.match, words))
 
 
 if __name__ == '__main__':
